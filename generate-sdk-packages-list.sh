1db66799d6126a01634ab059385c1f4824fae885
Alexey Pelykh <alexey.pelykh@gmail.com>
2018-03-28 20:34:55 +0300

    SDK packages list generator; Updated SDK packages list

---
+++ generate-sdk-packages-list.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+$ANDROID_SDK/tools/bin/sdkmanager --list --include_obsolete \
+  | awk '{print $1;}' \
+  | grep -E 'tools|build-tools|platform-tools|platforms|cmake|ndk-bundle|add-ons;addon-google_|extras|patcher' \
+  | grep -v 'extras;intel;Hardware_Accelerated_Execution_Manager' \
+  | sort -u \
+  > "$SRC_DIR/sdk-packages.list"
