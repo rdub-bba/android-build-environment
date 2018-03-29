ede215f8e46f65054f9b97a0621932270565b57d
Alexey Pelykh <alexey.pelykh@gmail.com>
2018-03-29 09:36:55 +0300

    Update

--- build.sh
+++ build.sh
@@ -2,8 +2,11 @@
 
 SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-DATE_TAG=`date "+%Y%m%d"`
+DATE_TAG="$2"
+if [ -z "$DATE_TAG" ]; then
+  DATE_TAG=`date "+%Y%m%d"`
+fi
+
 docker build \
-  -t android-build-environment:latest \
   -t android-build-environment:$DATE_TAG \
   "$SRC_DIR"
--- generate-sdk-packages-list.sh
+++ generate-sdk-packages-list.sh
@@ -4,7 +4,7 @@ SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 $ANDROID_SDK/tools/bin/sdkmanager --list --include_obsolete \
   | awk '{print $1;}' \
-  | grep -E 'tools|build-tools|platform-tools|platforms|cmake|ndk-bundle|add-ons;addon-google_|extras|patcher' \
+  | grep -E 'tools|build-tools|platform-tools|platforms|cmake|ndk-bundle|add-ons;addon-google_|extras' \
   | grep -v 'extras;intel;Hardware_Accelerated_Execution_Manager' \
   | sort -u \
   > "$SRC_DIR/sdk-packages.list"
