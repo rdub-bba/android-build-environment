746b5f1d1dbe8a21505d7b0ba21fa9376ffb13c6
Alexey Pelykh <alexey.pelykh@gmail.com>
2018-06-18 11:14:32 +0300

    New SDK packages list; Updated SDK packages list generator

--- generate-sdk-packages-list.sh
+++ generate-sdk-packages-list.sh
@@ -4,7 +4,16 @@ SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 $ANDROID_SDK/tools/bin/sdkmanager --list --include_obsolete \
   | awk '{print $1;}' \
-  | grep -E 'tools|build-tools|platform-tools|platforms|cmake|ndk-bundle|add-ons;addon-google_|extras' \
+  | grep -vE '\<system-images\>' \
+  | grep -vE '\<sources\>' \
+  | grep -vE '\<lldb\>' \
+  | grep -vE '\<extras;android;gapid\>' \
   | grep -v 'extras;intel;Hardware_Accelerated_Execution_Manager' \
+  | grep -v 'extras;google;simulators' \
+  | grep -v 'extras;google;webdriver' \
+  | grep -v 'docs' \
+  | grep -v 'emulator' \
+  | grep -v 'extras;google;auto' \
+  | grep -v 'extras;google;webdriver' \
   | sort -u \
   > "$SRC_DIR/sdk-packages.list"
